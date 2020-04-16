e724d131082fc7ab9941ebc12634dfb1834c3dc8
Tyler Nam <60115779+taehyun9202@users.noreply.github.com>
2020-04-16 13:44:52 -0400

    Update Survey.cs

--- Models/Survey.cs
+++ Models/Survey.cs
@@ -1,10 +1,16 @@
+using System.ComponentModel.DataAnnotations;
 namespace DojoSurvey.Models
 {
     public class Survey
     {
+        [Required]
+        [MinLength(2)]
         public string Name {get;set;}
+        [Required]
         public string Location{get;set;}
+        [Required]
         public string Language{get;set;}
+        [MaxLength(20)]
         public string Comment{get;set;}
     }
 }
