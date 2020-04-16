1ae040f9cf17947caee20e7cfdbe3ffc9a4239a8
Tyler <taehyun9202@hotmail.com>
2020-04-16 01:45:02 -0400

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using DojoSurvey.Models;
+
+namespace DojoSurvey.Controllers
+{
+    public class HomeController : Controller
+    {
+        public static List<string> SurveyInfo = new List<string>()
+        {
+        };
+        [HttpGet("")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost("submit")]    
+        public IActionResult Submit(Survey survey)
+        {
+            Survey survey1 = new Survey()
+            {
+                Name = survey.Name,
+                Location = survey.Location,
+                Language = survey.Language,
+                Comment = survey.Comment
+            };
+            return View("Result", survey1);
+        }
+
+        [HttpGet("result")]
+        public IActionResult Result(Survey survey1)
+        {
+            return View("Result", survey1);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
